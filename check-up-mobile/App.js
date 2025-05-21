89972e1b70021be07db635c8a20fd26fd13e92cb
ArlanD3ve <arland3v@gmail.com>
2025-05-21 17:58:29 -0300

    Chore: update imports

--- check-up-mobile/App.js
+++ check-up-mobile/App.js
@@ -1,15 +1,19 @@
 import Account from "./src/screens/account/account.jsx";
 import Login from "./src/screens/login/login.jsx";
-
 import AbaHome from "./src/screens/abHome/abahome.jsx";
+import AbaCalendar from "./src/screens/abaCalendar/abacalendar.jsx";
+import AbaProfile from "./src/screens/abaProfile/abaprofile.jsx";
+import Main from "./src/screens/main/main.jsx";
+
+import Services from "./src/screens/services/services.jsx";
 
 function App() {
 
 	return <>
-		<AbaHome />
+		<Services />
 	</ >
 
 }
 export default App
 
-//00:44 Aula 2
+//01:24 Aula 2
